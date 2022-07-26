b66cefea8545204e8feea32abbae5baa9612c1f1
kladnik <marko@usespeak.com>
2022-07-26 15:59:03 +0200

    Build change

--- loadTest.js
+++ loadTest.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const axios = require('axios');
 
-const URL = 'http://localhost:3000/test';
-const CONCURRENT_REQUESTS = 200;
+const URL = 'http://23.251.147.14/test';
+const CONCURRENT_REQUESTS = 500;
 
 const options = { method: 'GET', url: URL };
 let counter = 0;
